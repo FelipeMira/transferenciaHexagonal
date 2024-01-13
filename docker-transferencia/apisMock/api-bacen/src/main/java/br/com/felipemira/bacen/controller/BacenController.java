b0f55b85a6dd11a3caa96ac3c878fa4c73f932c3
Felipe Mira <felipe.n.mira@outlook.com>
2024-01-12 22:09:45 -0300

    add: add README.md with cookbook

--- docker-transferencia/apisMock/api-bacen/src/main/java/br/com/felipemira/bacen/controller/BacenController.java
+++ docker-transferencia/apisMock/api-bacen/src/main/java/br/com/felipemira/bacen/controller/BacenController.java
@@ -1,6 +1,6 @@
-package br.com.felipemira.controller;
+package br.com.felipemira.bacen.controller;
 
-import br.com.felipemira.model.TransferenciaBacenRequest;
+import br.com.felipemira.bacen.model.TransferenciaBacenRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- docker-transferencia/apisMock/api-cadastro/src/main/java/br/com/felipemira/cadastro/RestApiApplication.java
+++ docker-transferencia/apisMock/api-cadastro/src/main/java/br/com/felipemira/cadastro/RestApiApplication.java
@@ -1,10 +1,8 @@
-package br.com.felipemira;
+package br.com.felipemira.cadastro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 public class RestApiApplication {
 
